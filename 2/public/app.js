001120b522bc400a0a017cd4aac6c6a579c1bf19
hiroyuki.kojima <hiroyuki.kojima@fxss.co.jp>
2017-09-14 18:40:54 +0900

    complete 2100-5

--- 2/public/app.js
+++ 2/public/app.js
@@ -1,7 +1,17 @@
 'use strict';
 // 名前空間の定義
 var learnjs = {};
+
+learing.problemView = function() {
+  return $('<div class="problem-view">').text('Comming soon!');
+}
+
 learnjs.showView = function(hash) {
-  var problemView = $('<div class="problem-view">').text('Comming soon!');
-  $('.view-container').empty().append(problemView);
+  var routes = {
+    'problem-view': learnjs.problemView
+  };
+  var viewFn = routes[hash];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
 }
